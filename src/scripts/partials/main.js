e02c35ced7705ca08779278e9e8c588fe7f75059
Vedmaka <god.vedmaka@gmail.com>
2017-06-06 17:50:15 +0300

    some hovers and fixes for inline elements (made em inline blocks actually)

--- src/scripts/partials/main.js
+++ src/scripts/partials/main.js
@@ -1,14 +1,22 @@
 $(function(){
 
-    $('.popup-stroke').each(function(index, element) {
+    $('.popup-stroke').hover(
+        function() {
+            // Hover
+            var wrapper = $(this).find('.hover-image-wrapper');
+            var img = $(wrapper).find('img');
 
-        var wrapper = $(element).find('.hover-image-wrapper');
-        var img = $(wrapper).find('img');
+            console.log( $(this).width(), $(this).height(), img.width() );
 
-        console.log( $(element).width(), $(element).height(), img.width() );
+            wrapper.css('left', Math.floor($(this).width() / 2) - Math.floor( img.width() / 2) + 'px' );
+            wrapper.css('bottom', Math.floor( $(this).height() ) + 5 + 'px');
 
-        wrapper.css('left', Math.floor( ($(element).width() / 2) - ( img.width() )) + 'px' );
-
-    });
+            $(this).addClass('hovered');
+        },
+        function() {
+            // Blur
+            $(this).removeClass('hovered');
+        }
+    );
 
 });
