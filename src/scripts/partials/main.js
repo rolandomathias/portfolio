691e436cf8091d9604dfaca10d98749d3194e46f
Vedmaka <god.vedmaka@gmail.com>
2017-06-15 12:22:37 +0300

    no logs in scripts

--- src/scripts/partials/main.js
+++ src/scripts/partials/main.js
@@ -6,8 +6,6 @@ $(function(){
             var wrapper = $(this).find('.hover-image-wrapper');
             var img = $(wrapper).find('img');
 
-            console.log( $(this).width(), $(this).height(), img.width() );
-
             wrapper.css('left', Math.floor($(this).width() / 2) - Math.floor( img.width() / 2) + 'px' );
             wrapper.css('bottom', Math.floor( $(this).height() ) + 5 + 'px');
 
